f429bac59db544bfd3883307a9143d71c80a3bf3
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-20 09:00:26 -0300

    Add methods and attributes to class Book

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,33 @@
 public class Book {
+    private String tituloDoLivro;
+    private String autor;
+    private String codigoUnico;
+    private boolean disponivel;
+
+    public exibirDetalhes(String tituloDoLivro, String autor, String codigoUnico){
+        this.tituloDoLivro = tituloDoLivro;
+        this.autor = autor;
+        this.codigoUnico = codigoUnico;
+    }
+    public String getTituloDoLivro(){
+        return tituloDoLivro;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public String getCodigoUnico(){
+        return autor;
+    }
+    public emprestar(boolean disponivel){
+        this.disponivel = true;
+    }
+    public boolean getDisponivel(){
+        return true;
+    }
+    public devolver(boolean devolver){
+        this.disponivel = false;
+    }
+    public boolean getDevolver(){
+        return false;
+    }
 }
